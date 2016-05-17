c441c938a7c7c814d3d32717bc65d15c1cc0ca67
abde96 <abderrahmane.hassaine@iut-valence.fr>
2016-05-17 23:31:07 +0200

    Ajout d'une nouvelle librairie. Ajout des blocs try/catch. Suppression de l'ancienne librairie.

--- src/fr/iutvalence/info/dut/m2107/bdd/ConnexionDB.java
+++ src/fr/iutvalence/info/dut/m2107/bdd/ConnexionDB.java
@@ -1,35 +1,39 @@
 package fr.iutvalence.info.dut.m2107.bdd;
-import java.sql.*;
 
-import org.omg.CORBA.Request;
-
+import java.sql.DriverManager;
+import java.sql.Connection;
+import java.sql.SQLException;
 
 public class ConnexionDB {
 
-	
-	
-	
-	
-	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+  public static void main(String[] argv) {
 
-		
-		try {
-			Class.forName("com.mysql.jdbc.Driver");
-		} catch (ClassNotFoundException e) {
-			System.out.println("driver ok");
-			e.printStackTrace();
-		}
-		
-		try {
-			Connection con = DriverManager.getConnection("jdbc:mysql://gigondas/hassaina","hassaina","hassaina");
-		} catch (SQLException e) {
-			System.out.println("conncter a la bdd");
-			e.printStackTrace();
-		}
-		System.out.println("Connexion établie");
-		
-		
+	System.out.println("-------- Test de la connexion à MySQL------------");
+
+	try {
+		Class.forName("com.mysql.jdbc.Driver");
+	} catch (ClassNotFoundException e) {
+		System.out.println("où se trouve le driver?");
+		e.printStackTrace();
+		return;
 	}
+
+	System.out.println("Driver bien enregistré");
+	Connection connection = null;
+
+	try {
+		connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/goform","root", "");
+
+	} catch (SQLException e) {
+		System.out.println("Connexion échouée, vérifiez le lien");
+		e.printStackTrace();
+		return;
+	}
+
+	if (connection != null) {
+		System.out.println("L'accès à la base de données est ouvert");
+	} else {
+		System.out.println("Impossible d'établir la connexion à la base de données !");
+	}
+  }
 }
